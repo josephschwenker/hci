53441fe640540c155759be36a151a227228dc88d
Joseph Schwenker <jschwenker@uiowa.edu>
2021-03-26 20:18:35 -0500

    Skeleton code

---
+++ script.js
@@ -0,0 +1,31 @@
+let Model = {
+
+    dictionary: [],
+
+    initializeDictionary: function() {
+
+    },
+
+    currentlySelected: undefined,
+
+    state: "inactive",
+
+    states: ["inactive", "select", "move"],
+
+    actions: {
+
+        changeMode: function(newMode) {
+
+        },
+        select: function(index) {
+
+        },
+        move: function(originalPosition, newPosition) {
+
+        },
+        submit: function() {
+
+        }
+
+    }
+}
